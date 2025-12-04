3666ae5669dcde6fdaf23cf0e89ddcc3342abb2d
Prajwal Aradhya <manuaradhya07@gmail.com>
2025-12-04 20:38:59 +0000

    added footer, and minor changes

--- components/bookNowButton.tsx
+++ components/bookNowButton.tsx
@@ -7,7 +7,7 @@ const BookNowButton = () => {
 	return (
 		<button
 			onClick={handleClick}
-			className="bg-green-600 cursor-pointer hover:bg-green-700 active:bg-green-800 text-white p-2 rounded-lg hover:shadow-lg shadow-green-300 active:translate-y-1 duration-300 transition-all hover:-translate-y-[1px]"
+			className="bg-green-600 text-2xl cursor-pointer hover:bg-green-700 active:bg-green-800 text-white p-4 rounded-lg hover:shadow-lg shadow-green-300 active:translate-y-1 duration-300 transition-all hover:-translate-y-[1px] uppercase font-bold"
 		>
 			Book Now
 		</button>
--- components/homepage/infoCard.tsx
+++ components/homepage/infoCard.tsx
@@ -1,25 +1,5 @@
 "use client";
 
-import BookNowButton from "../bookNowButton";
-import { ScrollFade } from "../scrollReveal";
-
-const coachingMethods = [
-	"Using online platforms such as - Zoom, Microsoft Teams or Google Meet.",
-	"Telephone and/or Video Call",
-	"In person. (Subject to location).",
-];
-
-const bulletPoints = [
-	"Break free from old patterns.",
-	"Gain clarity about what you truly want in your personal or professional life.",
-	"Build confidence you can feel.",
-	"Create meaningful goals.",
-	"Develop healthier values.",
-	"Reconnect with yourself.",
-	"Turn thoughts into transitions.",
-	"Live with purpose and happiness.",
-];
-
 const InfoCard = () => {
 	return (
 		<div className="min-h-[80vh] flex flex-col gap-8 p-8 bg-[#f7fcf9]">
@@ -64,45 +44,6 @@ const InfoCard = () => {
 						work with you to create a plan tailored to your needs and to ensure
 						you get the very most out of what you require.
 					</p>
-				</div>
-			</div>
-			<div className="m-8 text-gray-700 text-justify col-span-5 lg:col-span-3 flex flex-col gap-8">
-				<div className="flex flex-col gap-2">
-					<h3 className="text-2xl font-bold">
-						Is My Coaching Strategy for You?
-					</h3>
-					<p className="text-lg">Yes, it is for you if you want to:</p>
-					<ul className="list-disc pl-8">
-						{bulletPoints.map((point, index) => (
-							<ScrollFade start="top 92%" key={index}>
-								<li className="text-lg">{point}</li>
-							</ScrollFade>
-						))}
-					</ul>
-					<h3 className="text-2xl font-bold">What You Can Expect</h3>
-					<p>
-						My coaching is practical, compassionate, and tailored to you.
-						Together we will uncover what’s holding you back, explore what’s
-						possible, and design a plan that supports long-term change, in a
-						space that’s safe and non-judgmental.
-					</p>
-					<h3 className="text-2xl font-bold">How Do I Coach?</h3>
-					<ul className="list-disc pl-8">
-						{/* make a list of const */}
-
-						{coachingMethods.map((method, index) => (
-							<ScrollFade start="top 92%" key={index}>
-								<li className="text-lg">{method}</li>
-							</ScrollFade>
-						))}
-					</ul>
-					<h3 className="text-2xl font-bold">Ready to Begin?</h3>
-					<p>Your transformation starts with a single conversation.</p>
-					<p>
-						Book your FREE clarity session and let’s explore what’s possible for
-						you!
-					</p>
-					<BookNowButton />
 				</div>
 			</div>
 		</div>
